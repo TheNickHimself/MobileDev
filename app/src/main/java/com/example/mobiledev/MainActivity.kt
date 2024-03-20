f611f75111531c064f1656ffae5bb69f8533509c
Nick <78423234+TheNickHimself@users.noreply.github.com>
2024-03-20 17:52:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mobiledev/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.example.mobiledev
+
+class MainActivity {
+}
---
+++ app/src/main/java/com/example/mobiledev/WeatherResponse.kt
@@ -0,0 +1,8 @@
+package com.example.mobiledev
+
+data class WeatherResponse(
+    val name: String,
+    val temp: Double,
+    val condition: String
+
+)
